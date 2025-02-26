7ed8d86eb6f10f428af46c23bc1ada6b52e55ff0
CCDevX <cavaleri.cristina.dev@gmail.com>
2025-02-26 20:58:44 +0100

    First implementation of the HTML SCSS template

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import "./style.scss";
+
+const todos = [
+  {
+    task: "Emmerder le monde",
+    done: false,
+  },
+  {
+    task: "Manger une pomme",
+    done: false,
+  },
+  {
+    task: "Faire du café",
+    done: true,
+  },
+];
+
+const displayTodo = () => {
+  const todosMode = todos.map();
+};
